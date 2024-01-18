4665ab618d55b2f760b24afa2595c398fbdf9af5
jordandotdev <jordanjdyydj@gmail.com>
2024-01-18 23:18:43 +0530

    bg and margin of button

--- src/app/Components/portfolio.js
+++ src/app/Components/portfolio.js
@@ -5,7 +5,7 @@ import Prashan2 from "../../../public/jpg2.jpg";
 
 export default function Portfolio() {
     return (
-        <div className="item-center">
+        <div className="item-center bg-white">
            <div className=" flex justify-center  mb-4 text-4xl p-24  items-center py-8 px-4 mx-auto max-w-screen-xl font-extrabold text-gray-900 dark:text-white">
             Portfolio 
            </div> 
@@ -63,7 +63,7 @@ export default function Portfolio() {
             </div>
         </div>
         
-        <div className="flex justify-center items-center mb-4 text-4xl  mx-auto max-w-screen-xl">
+        <div className="flex justify-center items-center p-4 text-4xl  mx-auto max-w-screen-xl">
                 <button className="flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                     Learn More
