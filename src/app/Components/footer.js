0264555c974ebc6fc7f6e7446d4adf0fd01f0b3d
jordandotdev <jordanjdyydj@gmail.com>
2024-01-18 22:08:58 +0530

    inital commit ```

---
+++ src/app/Components/footer.js
@@ -0,0 +1,36 @@
+export default function Footer(){
+    return(
+        <div>
+            
+
+        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <img src="https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f3ba.png" class="h-8" alt="Prashan Logo" />
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Prashan</span>
+                    </a>
+                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">Jordan Ferdinando™</a>. All Rights Reserved.</span>
+            </div>
+        </footer>
+
+
+        </div>
+    );
+}
---
+++ src/app/Components/portfolio.js
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import Prashan1 from "../../../public/jpg1.jpg";
+import Prashan2 from "../../../public/jpg2.jpg";
+
+
+export default function Portfolio() {
+    return (
+        <div className="item-center">
+           <div className=" flex justify-center  mb-4 text-4xl p-24  items-center py-8 px-4 mx-auto max-w-screen-xl font-extrabold text-gray-900 dark:text-white">
+            Portfolio 
+           </div> 
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-24  items-center py-8 px-4 mx-auto max-w-screen-xl">
+            <div>
+                <img
+                    className="h-auto max-w-full rounded-lg transform transition-transform duration-500 ease-in-out hover:scale-105"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                    alt="Portfolio Image 1"
+                />
+            </div>
+            <div>
+                <img
+                    className="h-auto max-w-full rounded-lg transform transition-transform duration-500 ease-in-out hover:scale-105"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                    alt="Portfolio Image 2"
+                />
+            </div>
+            <div className="relative w-full h-full transform transition-transform duration-500 ease-in-out hover:scale-105">
+            <Image
+                className="rounded-lg"
+                src={Prashan1}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="50% 0%"
+            />
+            </div>
+            <div>
+                <img
+                    className="h-auto max-w-full rounded-lg transform transition-transform duration-500 ease-in-out hover:scale-105"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+                    alt="Portfolio Image 11"
+                />
+            </div>
+            <div className="relative w-full h-full transform transition-transform duration-500 ease-in-out hover:scale-105">
+            <Image
+                className="rounded-lg"
+                src={Prashan2}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="50% 0%"
+            />
+            </div>
+            <div className="relative w-full h-full transform transition-transform duration-500 ease-in-out hover:scale-105">
+            <Image
+                className="rounded-lg"
+                src={Prashan1}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="50% 0%"
+            />
+            </div>
+        </div>
+        
+        <div className="flex justify-center items-center mb-4 text-4xl  mx-auto max-w-screen-xl">
+                <button className="flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Learn More
+                    </span>
+                </button>
+        </div>
+ 
+
+        
+
+        </div>
+    );
+}
