3adf0802bf01299bf96aa8ab9f34b36574588dd6
jordandotdev <jordanjdyydj@gmail.com>
2024-02-17 22:07:38 +0530

    addition of shadcn - network failiure

---
+++ src/app/Components/nav.client.js
@@ -0,0 +1,54 @@
+"use client";
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Nav() {
+  const links = [
+    {
+      path: '/',
+      name: 'Home',
+    },
+    {
+      path: '/about-me',
+      name: 'About Me',
+    },
+    {
+      path: '/expeditions',
+      name: 'Expeditions',
+    },
+    {
+      path: '/portfolio',
+      name: 'Portfolio',
+    },
+    {
+      path: '#',
+      name: 'Majestic Horns',
+    },
+    {
+      path: '#',
+      name: 'Reach Out',
+    },
+  ]
+  const pathname = usePathname();
+  return (
+<nav className="bg-white relative top-0 w-full z-50">
+  <div className="max-w-screen-xl flex flex-wrap justify-center items-center mx-auto p-4">
+   <ul className='flex gap-2'>
+    {links.map(link => {
+      return(
+        <li key={link.path}>
+          <Link className={cn("font-medium rounded-lg text-sm py-2 px-4 transition-all duration-500 ease-out hover:bg-slate-200",
+            pathname === link.path ? "bg-slate-300" : ""
+          )} href={link.path}>
+            <span>{link.name}</span>
+          </Link>
+        </li>
+      )
+    })}
+   </ul>
+  </div>
+</nav>
+
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from "./Components/footer.js";
-import Nav from '../app/Components/navComponent.js';
+import Nav from '../app/Components/nav.client.js';
 
 const inter = Inter({ subsets: ['latin'] })
 
