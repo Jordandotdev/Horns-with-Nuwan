17295d758b39e5ccd76bc4500938a014e502d586
jordandotdev <jordanjdyydj@gmail.com>
2024-02-17 18:24:40 +0530

    initial styling for the navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,45 +1,21 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Footer from "./Components/footer.js";
-import Nav from '../app/Components/navComponent.js';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Footer from "./Components/footerComponent.js";
+import Nav from './Components/navComponent.js';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Welcome',
   description: 'Welcome to my personal website!',
-}
+};
 
-function getPageValue(pageName) {
-  let value;
-  switch(pageName) {
-     case 'home':
-       value = 1;
-       break;
-     case 'portfolio':
-       value = 2;
-       break;
-     case 'contact':
-       value = 3;
-       break;
-     default:
-       value = 1;
-  }
-  console.log(`Page Name: ${pageName}, Page Value: ${value}`);
-  return value;
-}
-
- export default function RootLayout({ children, pageName }) {
-  const pageValue = getPageValue(pageName);
+export default function RootLayout({ children }) {
   return (
-     <html lang="en">
-       <body className={inter.className}>
-         <Nav pageValue={pageValue}/>
-         <main>{children}</main>
-         <Footer/>
-       </body>
-     </html>
-  )
- }
-
-
+    <div>
+      <Nav />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
