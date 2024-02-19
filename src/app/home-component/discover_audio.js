bdd5ce427ed880d5f025955f4f0e36e08de1cd88
jordandotdev <jordanjdyydj@gmail.com>
2024-02-19 13:53:56 +0530

    addition of card ui element shadcn

--- src/app/home-component/discover_audio.js
+++ src/app/home-component/discover_audio.js
@@ -3,7 +3,7 @@ import AudioPlayer from "./components/audio_player.js";
 export default function discover_audio() {
     return (
         <div>
-            <section className="bg-white px-4 py-8">
+            <section className="bg-white px-4 py-4">
                 <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                     <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
                         <h2 className="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">Discover</h2>
