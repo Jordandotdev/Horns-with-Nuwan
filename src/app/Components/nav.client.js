c8a9f7ba64d24105e1915a262228b69acc6fb1a7
jordandotdev <jordanjdyydj@gmail.com>
2024-02-23 14:56:24 +0530

    creation of a navcomponent in order to use the useState hook in react

--- src/app/Components/nav.client.js
+++ src/app/Components/nav.client.js
@@ -1,65 +1,11 @@
 "use client";
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../Components/ui/dropdown-menu.jsx"
+import { useState } from 'react';
+import NavComponent from './ui/nav';
 
 export default function Nav() {
-  const links = [
-    {
-      path: '/',
-      name: 'Home',
-    },
-    {
-      path: '/about-me',
-      name: 'Self Portrait',
-    },
-    {
-      path: '/expeditions',
-      name: 'Expeditions',
-    },
-    {
-      path: '/portfolio',
-      name: 'Portfolio',
-    },
-    {
-      path: '/#Majestic',
-      name: 'Majestic Horns',
-    },
-    {
-      path: '#Footer',
-      name: 'Reach Out',
-    },
-  ]
-  const pathname = usePathname();
+  
+  const {isActive, setIsActive} = useState(false);
   return (
-    
-<nav className="fixed top-0 w-full z-50">
-
-
-      <div className="max-w-screen-xl flex flex-wrap justify-center items-center mx-auto p-4">
-        <ul className='flex flex-col gap-2 bg-white backdrop-blur-md p-3 rounded-lg shadow-lg transition-all duration-500 ease-out hover:opacity-100 opacity-30 md:flex-row'>
-          {links.map(link => {
-            return (
-              <li key={link.path} className="md:inline-block">
-                <Link className={cn("font-medium rounded-lg text-sm py-2 px-4 transition-all duration-500 ease-out hover:bg-slate-200",
-                  pathname === link.path ? "bg-slate-300" : ""
-                )} href={link.path}>
-                  <span>{link.name}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </nav>
-
+       <NavComponent/>
   );
 }
---
+++ src/app/Components/ui/nav.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function NavComponent() {
+    const links = [
+        {
+          path: '/',
+          name: 'Home',
+        },
+        {
+          path: '/about-me',
+          name: 'Self Portrait',
+        },
+        {
+          path: '/expeditions',
+          name: 'Expeditions',
+        },
+        {
+          path: '/portfolio',
+          name: 'Portfolio',
+        },
+        {
+          path: '/#Majestic',
+          name: 'Majestic Horns',
+        },
+        {
+          path: '#Footer',
+          name: 'Reach Out',
+        },
+      ]
+      const pathname = usePathname();
+
+    return(
+        <nav className="fixed top-0 w-full z-50">
+
+
+      <div className="max-w-screen-xl flex flex-wrap justify-center items-center mx-auto p-4">
+        <ul className='flex flex-col gap-2 bg-white backdrop-blur-md p-3 rounded-lg shadow-lg transition-all duration-500 ease-out hover:opacity-100 opacity-30 md:flex-row'>
+          {links.map(link => {
+            return (
+              <li key={link.path} className="md:inline-block">
+                <Link className={cn("font-medium rounded-lg text-sm py-2 px-4 transition-all duration-500 ease-out hover:bg-slate-200",
+                  pathname === link.path ? "bg-slate-300 "  : ""
+                )} href={link.path}>
+                  <span>{link.name}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+    );
+}
