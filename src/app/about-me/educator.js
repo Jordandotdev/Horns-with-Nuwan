3bfb76421d47ed480da7bb7341f138884159c4cf
jordandotdev <jordanjdyydj@gmail.com>
2024-02-23 13:25:27 +0530

    changing of the hero image and the educator image

--- src/app/about-me/educator.js
+++ src/app/about-me/educator.js
@@ -1,19 +1,17 @@
 import Image from 'next/image';
-import Prashan1 from '../../../public/jpg1.jpg';
+import Educator from '../../../public/educator.jpg';
 
 export default function educator() {
     return(
         <section className="bg-white px-4 py-4 ">
     <div className="gap-16 items-center  px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2  lg:px-6">
     <div className="relative aspect-w-1 aspect-h-1 mt-2 md:mt-4">
-        <Image
-            className="rounded-lg"
-            src={Prashan1}
-            layout="responsive"
-            objectFit="cover"
-            quality={75}  
-            alt="Image description"  
-        />
+          <Image
+              className="rounded-lg h-auto max-h-[700px] object-cover object-top lg:max-w-[450px] "
+              src={Educator}
+              quality={75}   
+              alt="Image description"
+          />
         </div>
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">An Educator</h2>
--- src/app/home-component/hero.js
+++ src/app/home-component/hero.js
@@ -1,7 +1,7 @@
 export default function hero() {
     return (
         <div>
-            <section className=" w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Nuwan1.jpg')] bg-gray-700 bg-blend-multiply relative">
+            <section className=" w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Hero_background.jpg')] bg-gray-400 bg-blend-multiply relative">
                 <div className="absolute bottom-0 left-0 pb-4 px-10  max-w-screen-xl text-left   lg:pl-20 pr-60">
                     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl ">Hi im Nuwan !</h1>
                     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl ">
