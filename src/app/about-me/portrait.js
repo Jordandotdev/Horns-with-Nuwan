f56b97b7fb7c8a7cbccb1d4ea096b570c1cebed0
jordandotdev <jordanjdyydj@gmail.com>
2024-02-27 13:00:08 +0530

    passing the data from strapi into the front end

--- src/app/about-me/portrait.js
+++ src/app/about-me/portrait.js
@@ -25,14 +25,10 @@ return(
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">Biography</h2>
           <p className="mb-4">
-            Nuwan Gunawardhana is an Eductor, Recording Artist, Composer and multo-instrumentalist
-            who specializes in Trumpet, Trombone and Harmonica.He performs as a permanent member in two well established bands in Sri Lanka -
-            The A Team and the Outlaws, and is also a member of Gustav Mahler Orchestra of Colombo.
+              {portrait.data.attributes.Paragraph1}
           </p>
           <p>
-            Nuwan is currently an instructor to the National Youth Orchestra of Sri Lanka and as well
-            as an honorart consultant in music to the Sri Lanka Army. He also directed several school bands
-            in Sri Lanka & taught Western music at Sussex College, Kiribathgoda. 
+              {portrait.data.attributes.paragraph2} 
           </p>
           <Button className="mt-4">Download Resume</Button>
 
