36952b39b887fce09870738877d6f7fdc3971d32
jordandotdev <jordanjdyydj@gmail.com>
2024-03-01 13:28:07 +0530

    change of the title to biography

--- src/app/about-me/portrait.js
+++ src/app/about-me/portrait.js
@@ -22,7 +22,7 @@ return(
     <section className="bg-white px-4 py-4 mt-16">
     <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          <h2 className="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">ABout me</h2>
+          <h2 className="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">Biography</h2>
           <p className="mb-4">
               {portrait.data.attributes.Paragraph1}
           </p>
