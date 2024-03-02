ad78687b9d17fc51816124c432013fd9051e49bb
jordandotdev <jordanjdyydj@gmail.com>
2024-03-03 01:28:55 +0530

    cache issue with the backend fixed

--- src/app/action.js
+++ src/app/action.js
@@ -1,16 +1,17 @@
 import config from '../config.js';
 
 export const FetchProjects = async (page, items) => {
-    const pageNumber = Number(page);
-    const itemNumber = Number(items);
-    const reqOptions = {
-        headers: {
-          Authorization: `Bearer ${process.env.API_TOKEN}`,
-          'Cache-Control': 'no-store',
-        }
-    };
-    const response = await fetch(`${config.api}/api/projects?publicationState=live&locale[0]=en&pagination[pageSize]=${itemNumber}&pagination[page]=${pageNumber}`, reqOptions);
-    const projects = await response.json();
-    console.log('Projects: ', projects.data);
-    return projects;
+  const pageNumber = Number(page);
+  const itemNumber = Number(items);
+  const reqOptions = {
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      },
+      cache: 'no-store', 
+  };
+  const response = await fetch(`${config.api}/api/projects?publicationState=live&locale[0]=en&pagination[pageSize]=${itemNumber}&pagination[page]=${pageNumber}`, reqOptions);
+  const projects = await response.json();
+  console.log('Projects: ', projects.data);
+  return projects;
 };
--- src/app/portfolio/datatable.js
+++ src/app/portfolio/datatable.js
@@ -10,8 +10,8 @@ export default async function DataTable() {
   return (
     <section className="flex flex-col items-center">
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center py-4 px-4 mx-auto max-w-screen-l">
-        {projects.data.map((item) => (
-          <ProjectTemp key={item.id} project={item} />
+        {projects.data.map((item, index) => (
+          <ProjectTemp key={item.id} project={item} id={index}/>
         ))}
       </div>
       <Button className="mt-4">Load More</Button>
