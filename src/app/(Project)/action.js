bec192deb79357b0fe04e9dbffdff1a453c60935
jordandotdev <jordanjdyydj@gmail.com>
2024-03-21 13:04:40 +0530

    addition of local env file

--- src/app/(Project)/action.js
+++ src/app/(Project)/action.js
@@ -4,25 +4,29 @@ import React, { useState, useEffect } from 'react';
 import config from '../../config.js';
 import ProjectTemp from '../../app/Components/project_temp.js';
 
+
 export default function Projects({ items }) {
   const [posts, setPosts] = useState(null);
   const pageNumber = 1;
   const itemNumber = 6;
+  const reqOptions ={
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`
+    },
+    cache: 'no-store',
+  }
   
   const getProjects = async () => {
     try {
-      const response = await fetch(
+       const response = await fetch(
         `${config.api}/api/projects?populate=*&publicationState=live&locale[0]=en&pagination[pageSize]=${itemNumber}&pagination[page]=${pageNumber}`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.API_TOKEN_2}`
-          },
-          cache: 'no-store',
-        }
-      );
-      if (response) {
-        console.log('response:', response);
-      }else{
+        reqOptions,
+      )
+      if (response.ok) {
+        const data = await response.json();
+        setPosts(data);
+        console.log(data);
+      } else {
         throw new Error('Network response was not ok');
       }
     } catch (error) {
@@ -36,12 +40,11 @@ export default function Projects({ items }) {
 
   return (
     <>
-      {/* <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center py-4 px-4 mx-auto max-w-screen-lg">
-        {posts  && posts.map((props, index) => (
-          <ProjectTemp key={index} project={props} />
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center py-4 px-4 mx-auto max-w-screen-l">
+        {posts && posts.data && posts.data.map((props) => (
+          <ProjectTemp project={props} />
         ))}
-      </div> */}
-      <h1>{posts}</h1>
+      </div>
     </>
   );
 }
