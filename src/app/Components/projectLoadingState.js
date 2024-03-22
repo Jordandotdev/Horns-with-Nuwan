f5694fdcdbca5430ec98ef2f473e56db81af9efd
jordandotdev <jordanjdyydj@gmail.com>
2024-03-22 15:27:40 +0530

    initial addition of the image api

--- src/app/Components/projectLoadingState.js
+++ src/app/Components/projectLoadingState.js
@@ -1,15 +1,12 @@
 'use client';
 
 import { Button } from "./ui/button.jsx";
-import config from '../../config.js';
 
-const ProjectTemp = ({project}) => {
+const ProjectTemp = () => {
     return (
         <section className=" m-2 font-sans leading-normal flex h-auto">
         <div className="bg-white max-w-xl  shadow-lg rounded overflow-hidden m-4 sm:flex md:flex">
-          <div className="h-48 sm:h-auto sm:w-80  md:w-140 flex-wrap  rounded rounded-t sm:rounded sm:rounded-l text-center overflow-hidden bg-cover bg-center bg-no-repeat" >
-            
-             {/* backgroundImage: `url('${config}${project.Image.data.attributes.url}')`  */}
+          <div className="h-48 sm:h-auto sm:w-80  md:w-140 flex-wrap  rounded rounded-t sm:rounded sm:rounded-l text-center overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('https://unsplash.it/804/800')]" >
           </div>
            <div className="px-6 py-4 ">
             <h2 className="mb-2 font-black">
