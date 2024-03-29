65cef910e776f5cc8613962ac9c6088dcea3c538
jordandotdev <jordanjdyydj@gmail.com>
2024-03-29 22:25:21 +0530

    refactor: addition of the quote in the homepage/ hero

--- src/app/home-component/hero.js
+++ src/app/home-component/hero.js
@@ -4,7 +4,7 @@ export default function hero() {
             <section className=" w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Hero_background.jpg')] bg-gray-400 bg-blend-multiply relative">
                 <div className="absolute bottom-0 left-0 pb-4 px-10  max-w-screen-xl text-left   lg:pl-20 pr-60">
                     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl ">
-                    A true musician is someone who nourishes the soul with music and spreads love and joy through it. I am a true musician and I am here to spread love and joy through my music.
+                    A true musician is someone who nourishes the souls of people with the music created with a purpose, and his journey should ideally be to become an artist with a distinct sound and a professional with great versatility
                     </p>
                 </div>
             </section>
