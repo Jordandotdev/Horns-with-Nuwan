4a58106a217fd873dabe3749d4d1195624f8f047
jordandotdev <jordanjdyydj@gmail.com>
2024-03-30 22:14:29 +0530

    refactor: styling of the quote section

--- src/app/home-component/hero.js
+++ src/app/home-component/hero.js
@@ -1,9 +1,9 @@
 export default function hero() {
     return (
         <div>
-            <section className=" w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Hero_background.jpg')] bg-gray-400 bg-blend-multiply relative">
-                <div className="absolute bottom-0 left-0 pb-4 px-10  max-w-screen-xl text-left   lg:pl-20 pr-60">
-                    <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl ">
+            <section className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Hero_background.jpg')] bg-gray-400 bg-blend-multiply relative">
+                <div className="flex flex-col justify-end pb-4 px-10 max-w-screen-xl text-left lg:pl-20 h-full">
+                    <p className="mb-8 text-base sm:text-lg font-normal text-gray-300 lg:text-xl">
                     A true musician is someone who nourishes the souls of people with the music created with a purpose, and his journey should ideally be to become an artist with a distinct sound and a professional with great versatility
                     </p>
                 </div>
