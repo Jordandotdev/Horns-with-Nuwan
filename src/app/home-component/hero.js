ef2901bd1feec54379fe25fbff9441d61f9a1185
PrashanBastiansz <prashan@linearsix.com>
2024-04-01 12:17:43 +0530

    Removed image filter on home screen

--- src/app/home-component/hero.js
+++ src/app/home-component/hero.js
@@ -1,7 +1,7 @@
 export default function hero() {
     return (
         <div>
-            <section className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Hero_background.jpg')] bg-gray-400 bg-blend-multiply relative">
+            <section className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('../../public/Hero_background.jpg')] relative">
                 <div className="flex flex-col justify-end pb-4 px-10 max-w-screen-xl text-left lg:pl-20 h-full">
                     {/* <p className="mb-8 text-base sm:text-lg font-normal text-gray-300 lg:text-xl">
                     A true musician is someone who nourishes the souls of people with the music created with a purpose, and his journey should ideally be to become an artist with a distinct sound and a professional with great versatility
