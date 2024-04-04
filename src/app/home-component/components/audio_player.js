dffcaa097ba0d557b5b74a7538b3fae26a907f52
jordandotdev <jordanjdyydj@gmail.com>
2024-04-04 09:27:31 +0530

    refactor: audio component

--- src/app/home-component/components/audio_player.js
+++ src/app/home-component/components/audio_player.js
@@ -20,20 +20,22 @@ const AudioPlayer = () => {
            
             
             <div className="flex flex-col p-5">
-                <a href='https://youtu.be/_H7dlHRRD20'>
+                
                     <div className="flex border-b py-3 cursor-pointer hover:shadow-md px-2 ">
                         <iframe className='w-10 h-10' src="https://www.youtube.com/embed/_H7dlHRRD20" title="Little Sunflower Trumpet solo by Freddie Hubbard" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                        <div className="flex flex-col px-2 w-full">
-                            
-                            <span className="text-sm text-gold capitalize font-semibold pt-1">
-                            Little Sunflower Trumpet solo    
-                            </span>
-                            <span className="text-xs text-gray-500 uppercase font-medium ">
-                            Freddie Hubbard - Horns with Nuwan
-                            </span>
-                        </div>
+                        <a href='https://youtu.be/_H7dlHRRD20'>
+                            <div className="flex flex-col px-2 w-full">
+                                
+                                <span className="text-sm text-gold capitalize font-semibold pt-1">
+                                Little Sunflower Trumpet solo    
+                                </span>
+                                <span className="text-xs text-gray-500 uppercase font-medium ">
+                                Freddie Hubbard - Horns with Nuwan
+                                </span>
+                            </div>
+                        </a>
                     </div>
-                </a>
+                
                  <div className="flex border-b py-3 cursor-pointer hover:shadow-md px-2 ">
                     <Image  className='w-10 h-10 object-cover rounded-lg' alt='User avatar'src={AlbumArt1}/>
                     <div className="flex flex-col px-2 w-full">
