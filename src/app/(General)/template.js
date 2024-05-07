8883ab4f4639f8a7f5a397fc761e8cae51972d22
jordandotdev <jordanjdyydj@gmail.com>
2024-05-07 23:49:04 +0530

    Refactor: changing the Loading Anim and the duration

--- src/app/(General)/template.js
+++ src/app/(General)/template.js
@@ -5,25 +5,25 @@ import { LoadAnim } from "../../lib/loadinganim.js";
 
 export default function Template({ children }) {
   useEffect(() => {
-    const delayDuration = 5;
+    const delayDuration = 8;
     LoadAnim(delayDuration);
   }, []);
 
   return (
     <div>
-      <div id="load" className="fixed top-0 z-50 h-screen bg-gray-200">
+      <div id="load" className="fixed top-0 z-50 h-screen bg-amber-50">
         <div className="flex flex-col flex-grow items-center justify-center align-middle text-center h-full">
-          <div className="text-lg font-bold p-6 md:px-20 md:text-xl lg:p-40 lg:text-3xl">
+          <div className="text-lg font-thin p-6 md:px-20 md:text-xl lg:p-40 lg:text-3xl">
             “A true musician is someone who nourishes the souls of people with
             the music created with a purpose, and his journey should ideally be
             to become an artist with a distinct sound and a professional with
             great versatility.”
           </div>
           <div>
-            <div class="flex flex-row gap-2">
-              <div class="w-4 h-4 rounded-full bg-gray-700 animate-bounce"></div>
-              <div class="w-4 h-4 rounded-full bg-gray-700 animate-bounce [animation-delay:-.3s]"></div>
-              <div class="w-4 h-4 rounded-full bg-gray-700 animate-bounce [animation-delay:-.5s]"></div>
+            <div className="flex flex-row gap-2">
+              <div className="w-4 h-4 rounded-full bg-gray-700 animate-bounce"></div>
+              <div className="w-4 h-4 rounded-full bg-gray-700 animate-bounce [animation-delay:-.3s]"></div>
+              <div className="w-4 h-4 rounded-full bg-gray-700 animate-bounce [animation-delay:-.5s]"></div>
             </div>
           </div>
         </div>
--- src/lib/loadinganim.js
+++ src/lib/loadinganim.js
@@ -7,13 +7,14 @@ if (loadP){
     const tl =gsap.timeline()
 
     tl.set(loadP,{
-        yPercent:0,
+        opacity:1,
     }).to(loadP,{
-        yPercent:100,
         opacity:0,
         duration:1,
         ease:"power1.inOut",
         delay:delayDuration,
+    }).to(loadP,{
+        display:"none",
     })
 }
 }
