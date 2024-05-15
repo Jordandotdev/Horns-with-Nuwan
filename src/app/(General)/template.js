d5cbbc40f27ec045827a57eac5ade2599b0b2e86
jordandotdev <jordanjdyydj@gmail.com>
2024-05-15 10:57:41 +0530

    Refactor: change of styles for the pre loader for the styles of the paragraph

--- src/app/(General)/template.js
+++ src/app/(General)/template.js
@@ -13,7 +13,8 @@ export default function Template({ children }) {
     <div>
       <div id="load" className="fixed top-0 z-50 h-screen bg-amber-50">
         <div className="flex flex-col flex-grow items-center justify-center align-middle text-center h-full">
-          <div className=" text-lg text-gray-600 font-thin p-6 md:px-20 md:text-xl lg:p-40 lg:text-3xl">
+          {/* make the loader a fixed size in the center of the screen */}
+          <div className=" text-lg text-gray-600 font-thin p-6 md:px-20 md:text-xl lg:p-40 lg:text-2xl">
             “A true musician is someone who nourishes the souls of people with
             the music created with a purpose, and his journey should ideally be
             to become an artist with a distinct sound and a professional with
