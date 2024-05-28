913bae73a41566713db8d1adf8c70209ac885435
jordandotdev <jordanjdyydj@gmail.com>
2024-05-29 01:19:16 +0530

    Refactor: initial addition of the filter

--- src/app/(Project)/portfolio/page.js
+++ src/app/(Project)/portfolio/page.js
@@ -1,11 +1,15 @@
 'use client';
 
 import  Projects  from '../action.js';
+import Filter from '../../Components/filter.js';
 
 const Portfolio = () => {
 
   return (
     <section className="mt-14 bg-offwhite px-1 md:px-4 py-4 flex flex-col justify-center"> 
+    <div className="flex justify-end mr-28">
+      <Filter/>
+      </div>
      <Projects/>
     </section>
   )
