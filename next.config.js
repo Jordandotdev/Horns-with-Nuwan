556c35cf762e4a47d44674ab94746519a9e447ef
jordandotdev <jordanjdyydj@gmail.com>
2024-09-04 15:58:30 +0530

    Refactor: update API tokens and add new API endpoint

--- next.config.js
+++ next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'www.facebook.com'
             },
+            {
+                protocol: 'https',
+                hostname: 'drive.google.com/drive/folders/1FCW4HYQdh5Kv4-NrN3-7acXAMrfe4UMt?usp=sharing'
+            }
         ],
     },
 }
