03a2c38ce41506baff57e1f89e23143fc1dd698b
jordandotdev <jordanjdyydj@gmail.com>
2024-09-08 14:02:30 +0530

    Refactor: Delay Duration 3S

--- src/app/(General)/template.js
+++ src/app/(General)/template.js
@@ -5,7 +5,7 @@ import { LoadAnim } from "../../lib/loadinganim.js";
 
 export default function Template({ children }) {
   useEffect(() => {
-    const delayDuration = 8;
+    const delayDuration = 3;
     LoadAnim(delayDuration);
   }, []);
 
